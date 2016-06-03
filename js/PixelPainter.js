39ef83c338ed888a333721822e712047b8a933d8
javenkn <javenkn7@yahoo.com>
2016-06-03 00:46:57 -1000

    finished all of the tests except for the last test

---
+++ js/PixelPainter.js
@@ -0,0 +1,68 @@
+function createGrid(rows, columns, attributes){
+  //checks if the rows passed in are negative, is a number or if it's NaN
+  //if it is then it throws an error
+  if(rows < 0 || typeof rows !== "number" || isNaN(rows)){
+    throw new Error("Rows must be a positive number");
+  }
+  //checks if the parameter "columns" is an object
+  //if it is then it sets the global variable attributes to columns
+  if(typeof columns === "object"){
+    attributes = columns;
+  }
+
+  //checks if the columns is undefined or is NaN
+  //if it is then it sets the columns to the rows
+  if(columns === undefined || isNaN(columns)){
+    columns = rows;
+  }
+
+  //creates a HUGE grid div
+  var divGrid = document.createElement("DIV");
+
+  //this for loop goes through the passed in parameter "rows" and creates divs
+  //for the amount of rows needed
+  for(var i = 0; i < rows; i++){
+    var divRows = document.createElement("DIV");
+
+    //if attributes is an object then
+    //it sets the attributes inside each row div that is created
+    addAttributes(divRows, attributes);
+
+    //I set the attribute of each row to an id of divRow number for counting
+    //purposes
+    divRows.setAttribute("id", "divRow" + i);
+
+    //after a divRow is created it gets appended into the HUGE div grid
+    divGrid.appendChild(divRows);
+
+    //this for loop goes through the passed in parameter "columns" and creates
+    //divs for the amount of columns needed
+    for(var j = 0; j < columns; j++){
+      var divColumns = document.createElement("DIV");
+
+      //if attributes is an object then
+      //it sets the attributes inside each row div that is created
+      addAttributes(divColumns, attributes);
+
+      //I set the attribute of each row to an id of divRow number for counting
+      //purposes
+      divColumns.setAttribute("id", "divColumn" + j);
+      //after a divRow is created it gets appended into the HUGE div grid
+      divRows.appendChild(divColumns);
+    }
+  }
+
+  return divGrid;
+}
+
+//if there are attributes that need to be added then it runs this function
+function addAttributes(divs, attributes){
+  if(typeof attributes === "object"){
+    var attributesKeys = Object.keys(attributes);
+    for(var i = 0; i < attributesKeys.length; i++){
+      divs.setAttribute(attributesKeys[i], attributes[attributesKeys[i]]);
+    }
+  }else{
+    return;
+  }
+}
---
+++ js/pixelpainter.spec.js
@@ -0,0 +1,106 @@
+var expect = chai.expect;
+
+describe('createGrid', function(){
+  it('should be a function called createGrid', function(){
+    expect(createGrid).to.be.a('function'); // typeof
+    expect(createGrid).to.be.an.instanceof(Function); // instanceof
+  });
+
+  var grid = createGrid(1,10);
+  var grid10 = createGrid(10,10);
+  it('should return a HTML element', function(){
+    expect(grid.tagName).to.equal("DIV");
+  });
+
+  describe('rows parameter', function(){
+    it('should accept non-negative values', function(){
+      expect(createGrid.bind(null, '')).to.throw();
+      expect(createGrid.bind(null, {})).to.throw();
+      expect(createGrid.bind(null, -1)).to.throw();
+      expect(createGrid.bind(null, 1)).to.not.throw();
+    });
+
+    it('should return a HTML element', function(){
+      expect(grid.tagName).to.equal("DIV");
+    });
+
+    it('should return an HTML Element with 1 child', function(){
+      expect(grid.children.length).to.equal(1);
+    });
+
+    it('should return an HTML Element with 10 children', function(){
+      expect(grid10.children.length).to.equal(10);
+    });
+  });
+
+  describe('columns parameter', function(){
+    it('should return an html element with 1 row, each with 1 column.', function(){
+      var grid = createGrid(1);
+      expect(grid.children.length).to.equal(1);
+    });
+
+    it('should return an html element with 10 rows, each with 10 columns.', function(){
+      var grid = createGrid(10);
+      expect(grid.children.length).to.equal(10);
+    });
+
+    it('should return an html element with 1 row, each with 10 columns.', function(){
+      var grid = createGrid(1, 10);
+      for(var i = 0; i < grid.children.length; i++){
+        expect(grid.children[i].children.length).to.equal(10);
+      }
+    });
+
+    it('should return an html element with 10 rows, each with 5 columns.', function(){
+      var grid = createGrid(10, 5);
+      for(var i = 0; i < grid.children.length; i++){
+        expect(grid.children[i].children.length).to.equal(5);
+      }
+    });
+  });
+
+  describe('attributes parameter', function(){
+  //If no column value is provided, all attributes should get applied to each
+  //grid element
+    it('should return an html element with 1 row, each with 1 column, each with no attributes.', function(){
+      var noAttributes = createGrid(1, {});
+      expect(noAttributes.children.length).to.equal(1);
+      for(var i = 0; i < noAttributes.children.length; i++){
+        expect(noAttributes.children[i].children.length).to.equal(1);
+      }
+    });
+
+    it('should return an html element with 10 rows, each with 10 columns, each with the class of grid.', function(){
+      var gridAttributes = createGrid(10, {class: 'grid'});
+      for(var i = 0; i < gridAttributes.children.length; i++){
+        //rows
+        expect(gridAttributes.children.length).to.equal(10);
+        //columns
+        expect(gridAttributes.children[i].children.length).to.equal(10);
+
+        expect(gridAttributes.children[i].getAttribute("class")).to.equal("grid");
+        for(var j = 0; j < gridAttributes.children[i].children.length; j++){
+          expect(gridAttributes.children[i].children[j].getAttribute("class")).to.equal("grid");
+        }
+      }
+    });
+
+    it('should return an html element with 10 rows, each with 10 columns, each with the class of grid and style of background-color: black.', function(){
+      var grid = createGrid(10, {class: 'grid', style: 'background-color: black'});
+      for(var i = 0; i < grid.children.length; i++){
+        //rows
+        expect(grid.children.length).to.equal(10);
+        //columns
+        expect(grid.children[i].children.length).to.equal(10);
+        //attributes
+        expect(grid.children[i].getAttribute("class")).to.equal("grid");
+        expect(grid.children[i].getAttribute("style")).to.equal("background-color: black");
+        for(var j = 0; j < grid.children[i].children.length; j++){
+          //attributes
+          expect(grid.children[i].children[j].getAttribute("class")).to.equal("grid");
+          expect(grid.children[i].children[j].getAttribute("style")).to.equal("background-color: black");
+        }
+      }
+    });
+  });
+});
