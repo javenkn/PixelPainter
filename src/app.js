3f90b2e5d09c6ffd55bcc7d8625c13bcfc5547f7
javenkn <javenkn7@yahoo.com>
2016-09-10 15:41:47 -1000

    added pixel painter app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { PixelPainterApp } from './containers';
 
 ReactDOM.render(
-  <div>
-    { 'Welcome to PIXEL PAINTER REDUX!' }
-  </div>,
+  <PixelPainterApp />,
   document.getElementById('app')
 );
