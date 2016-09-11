dc1dbe970c5d78126d2ddd8b1ceeb28d0bb6910f
javenkn <javenkn7@yahoo.com>
2016-09-10 14:30:22 -1000

    initial commit on redux branch

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+ReactDOM.render(
+  <div>
+    { 'Welcome to PIXEL PAINTER REDUX!' }
+  </div>,
+  document.getElementById('app')
+);
